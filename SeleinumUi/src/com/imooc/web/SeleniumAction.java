7886eadfced56dd51e286e5d64585e77653e8bbe
Linzi <1151953214@qq.com>
2022-03-31 22:46:38 +0800

    redio元素输入框相关操作

--- SeleinumUi/src/com/imooc/web/SeleniumAction.java
+++ SeleinumUi/src/com/imooc/web/SeleniumAction.java
@@ -5,12 +5,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class SeleniumAction {
     WebDriver driver;
     public void InitDriver(){
 
         System.setProperty("webdriver.chrome.driver","D:\\Study\\chromdriver\\chromedriver.exe");
         driver = new ChromeDriver();
+
         driver.get("https://www.imooc.com/user/newlogin/from_url/1005/");
     }
 //    input输入元素操作
@@ -22,9 +25,19 @@ public class SeleniumAction {
         String UserInfo = EmeailElement.getAttribute("placeholder");
 //        输入账号
         EmeailElement.sendKeys("18410152358");
+
+//        输入密码
+        driver.findElement(By.name("password")).sendKeys("lin18410152358");
 //        等待三秒
         try {
-            Thread.sleep(3000);
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+//        点击登录
+        driver.findElement(By.className("moco-btn-red")).click();
+        try {
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -34,9 +47,9 @@ public class SeleniumAction {
 //        打印出来上方placeholder元素的内容
         System.out.println(UserInfo);
 //      取出input输入框的内容
-        String Mobel = EmeailElement.getAttribute("value");
+ //       String Mobel = EmeailElement.getAttribute("value");
 //        打印输入框出输入框的内容
-        System.out.println(Mobel);
+  //      System.out.println(Mobel);
 //        isEnabled()方法判断输入框是否可以输入
         boolean info = EmeailElement.isEnabled();
 //        打印状态
@@ -51,9 +64,11 @@ public class SeleniumAction {
 
     }
 
+
     public static void main(String[] args) {
         SeleniumAction  seleniumaction = new SeleniumAction();
         seleniumaction.InitDriver();
         seleniumaction.InputElement();
+
     }
 }
---
+++ SeleinumUi/src/com/imooc/web/SeleniumRedio.java
@@ -0,0 +1,64 @@
+package com.imooc.web;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class SeleniumRedio {
+//    redio相关操作
+        WebDriver driver;
+    public void InitDriver(){
+        System.setProperty("webdriver.chrome.driver","D:\\Study\\chromdriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.imooc.com/user/newlogin/from_url/1005/");
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+//        打开个人中心页
+        driver.get("https://www.imooc.com/user/setprofile");
+    }
+
+    public void Redio(){
+//        打开个人中心页
+        //driver.get("https://www.imooc.com/user/setprofile");
+
+
+//        点击编辑按钮
+        driver.findElement(By.className("js-edit-info")).click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+//            拿到父级表单
+        WebElement UserForm = driver.findElement(By.id("profile"));
+        //        根据父级去找里面的sex
+        List<WebElement> SexList = UserForm.findElements(By.name("sex"));
+        //        循环列表 点击
+        for(WebElement sex:SexList){
+            //            判断sex是否被选中 isSelected()方法判断哪是否选中
+            if (sex.isSelected()){
+                break;
+            }else {
+                sex.click();
+            }
+        }
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.close();
+    }
+
+    public static void main(String[] args) {
+        SeleniumRedio selenium_redio = new SeleniumRedio();
+        selenium_redio.InitDriver();
+        selenium_redio.Redio();
+    }
+}
