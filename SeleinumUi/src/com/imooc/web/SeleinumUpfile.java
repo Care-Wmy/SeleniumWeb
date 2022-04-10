ec80f469e19c1f296ab139dc2b1847692fb2ba3a
Linzi <1151953214@qq.com>
2022-04-10 22:31:43 +0800

    上传文件相关操作

--- SeleinumUi/src/com/imooc/web/SeleinumUpfile.java
+++ SeleinumUi/src/com/imooc/web/SeleinumUpfile.java
@@ -7,6 +7,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 
 public class SeleinumUpfile {
     WebDriver driver;
@@ -59,12 +63,52 @@ public class SeleinumUpfile {
         }
         driver.close();
     }
+    public void UpFileTwo() throws AWTException {
 
-    public static void main(String[] args) {
+        driver.get("https://www.imooc.com/user/setbindsns");
+//        移动到头像处弹出浮窗
+        WebElement HeadPng = driver.findElement(By.className("avator-mode"));
+        Actions action = new Actions(driver);
+        action.moveToElement(HeadPng).perform();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+//        点击更换头像
+        driver.findElement(By.className("js-avator-link")). click();
+        driver.findElement(By.className("avator-btn-fake")).click();
+//        打开上传文件框
+        StringSelection Selectjpg = new StringSelection("C:\\Users\\Linzi\\Desktop\\456.jpg");
+         Clipboard Sysc = Toolkit.getDefaultToolkit().getSystemClipboard();
+         Sysc.setContents(Selectjpg,null);
+//         进行ctrl+v操作 把上面文件地址粘贴到文件框地址栏
+        Robot robot = new Robot();
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.close();
+    }
+    public static void main(String[] args) throws AWTException {
         SeleinumUpfile seleinumUpfile = new SeleinumUpfile();
         seleinumUpfile.InitDriver();
         seleinumUpfile.DengLu();
-        seleinumUpfile.UpFile();
+//        seleinumUpfile.UpFile();
+        seleinumUpfile.UpFileTwo();
     }
 
 }
