797b479d140401f57b6f06b607f47733605eafa1
Linzi <1151953214@qq.com>
2022-04-10 22:54:03 +0800

    select方法操作下拉框

--- SeleinumUi/src/com/imooc/web/SeleniumWebFrom.java
+++ SeleinumUi/src/com/imooc/web/SeleniumWebFrom.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 
@@ -34,7 +35,7 @@ public class SeleniumWebFrom {
             e.printStackTrace();
         }
     }
-
+//  下拉框相关操作
     public void SelectOption(){
 //        打开个人中心页
          driver.get("https://www.imooc.com/user/setprofile");
@@ -59,12 +60,39 @@ public class SeleniumWebFrom {
         }
         driver.close();
     }
+//    下拉框相关操作2
+    public void SelectOptionTwo(){
+        //        打开个人中心页
+        driver.get("https://www.imooc.com/user/setprofile");
+
+//        点击编辑按钮
+        driver.findElement(By.className("js-edit-info")).click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+//            拿到父级表单
+        WebElement UserForm = driver.findElement(By.id("profile"));
+//        拿到下拉框
+        WebElement JobList = UserForm.findElement(By.id("job"));
+//     运用select方法操作下拉框
+      Select DowenList = new Select(JobList);
+      DowenList.selectByIndex(8);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.close();
+    }
 
     public static void main(String[] args) {
         SeleniumWebFrom seleniumWebFrom = new SeleniumWebFrom();
         seleniumWebFrom.InitDriver();
         seleniumWebFrom.WebForm();
-        seleniumWebFrom.SelectOption();
+ //       seleniumWebFrom.SelectOption();
+        seleniumWebFrom.SelectOptionTwo();
     }
 }
 
