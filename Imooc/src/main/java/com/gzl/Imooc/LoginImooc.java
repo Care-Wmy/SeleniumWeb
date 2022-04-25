8608403f9bfc79ba5f3dcc23b279b27b7858c2a4
Linzi <1151953214@qq.com>
2022-04-25 23:17:25 +0800

    重构封装登录方法参数化

--- Imooc/src/main/java/com/gzl/Imooc/LoginImooc.java
+++ Imooc/src/main/java/com/gzl/Imooc/LoginImooc.java
@@ -1,6 +1,6 @@
 package com.gzl.Imooc;
 
-import com.gzl.Imooc.AppTest;
+import com.gzl.Imooc.util.ProUtil;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -18,7 +18,7 @@ public class LoginImooc {
     }
 
     public void  UserLogin(){
-        String UserElement = "email";
+/*        String UserElement = "email";
         String GetByName = "name";
 
         String PassElement = "password";
@@ -31,16 +31,15 @@ public class LoginImooc {
         String UserPng = "js-header-avator";
 
         String GetByUserNames = "className";
-        String UserNames = "text-ellipsis";
-
+        String UserNames = "text-ellipsis";*/
         String UserName = "18410152358";
         String PassWord = "lin18410152358";
 //        定位账号输入框
-        WebElement EmeailElement = GetElenent(GetByName,UserElement);
+        WebElement EmeailElement = GetElenent("username");
 //        定位密码输入框
-        WebElement PassWordElement = GetElenent(GetByPassWord,PassElement);
+        WebElement PassWordElement = GetElenent("password");
 //        定位登录按钮
-        WebElement LoginClick = GetElenent(GetByLoginElement,LoginElement);
+        WebElement LoginClick = GetElenent("loginbutton");
 //         输入账号
         EmeailElement.sendKeys(UserName);
 //         输入密码
@@ -54,12 +53,12 @@ public class LoginImooc {
         }
         try{
 //            定位头像框元素
-            WebElement UserPngs = GetElenent(GetByUserPng,UserPng);
+            WebElement UserPngs = GetElenent("headpng");
             Actions actions = new Actions(driver);
 //            鼠标悬停
             actions.moveToElement(UserPngs).perform();
 //            获取用户名
-            String UserNameInfo = GetElenent(GetByUserNames,UserNames).getText();
+            String UserNameInfo = GetElenent("userinfo").getText();
 //            判断用户名是否匹配
             if (UserNameInfo.equals("稳妥_1")){
                 System.out.println("登录成功" + UserNameInfo);
@@ -73,26 +72,33 @@ public class LoginImooc {
         driver.close();
     }
 
-    public By GetByLocal(String getby, String getValue){
-        if (getby.equals("id")){
-            return By.id(getValue);
-        }else if (getby.equals("name")){
-            return By.name(getValue);
-        }else if (getby.equals("className")){
-            return By.className(getValue);
+    public By GetByLocal(String key){
+        ProUtil proUtil = new ProUtil("D:\\Study\\JavaUi\\SeleniumWeb\\Imooc\\element.properties");
+        String Locator = proUtil.GetPorperties(key);//key等于配置文件中的username=name>email
+//        取第一个值，用">"分割
+        String LocatorBy = Locator.split(">")[0];
+//        //        取第二个值
+        String LocatorValue = Locator.split(">")[1];
+
+        if (LocatorBy.equals("id")){
+            return By.id(LocatorValue);
+        }else if (LocatorBy.equals("name")){
+            return By.name(LocatorValue);
+        }else if (LocatorBy.equals("className")){
+            return By.className(LocatorValue);
         }else{
-            return By.xpath(getValue);
+            return By.xpath(LocatorValue);
         }
     }
 
-    public WebElement GetElenent(String getby, String getValue){
-        WebElement Element =  driver.findElement(this.GetByLocal(getby,getValue));
+    public WebElement GetElenent(String key){
+        WebElement Element =  driver.findElement(this.GetByLocal(key));
         return Element;
     }
 
     public static void main(String[] args) {
-        AppTest appte =  new AppTest();
-        appte.InitDriver();
-        appte.UserLogin();
+        LoginImooc loginImooc =  new LoginImooc();
+        loginImooc.InitDriver();
+        loginImooc.UserLogin();
     }
 }
